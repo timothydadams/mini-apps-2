0c38f77d40b7ab27faff73c26c448938ac3a9f69
Tim Adams <adams.timothy.d@gmail.com>
2020-06-09 11:52:39 -0700

    Basic setup complete

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 
+const App = () => {
+  return (
+    <div>React is working!</div>
+  )
+}
 
+ReactDom.render(<App />, document.getElementById('app'));
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'app.jsx'),
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
+};
